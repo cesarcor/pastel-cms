e3fb0109c895b96fcc7f04e45ce5123eae2db14f
Cesar Correchel <cesarcorrechel@gmail.com>
2019-03-03 23:11:23 -0600

    add create story button component

---
+++ src/components/Stories/CreateStoryButton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const createStoryButton = props => {
+
+    return(
+      <div className = "create-story">
+
+        <button>
+          <span>+</span>
+          <span>Create Story</span>
+        </button>
+
+
+      </div>
+    );
+
+}
+
+export default createStoryButton;
