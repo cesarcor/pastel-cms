5e4eba7894af0f55c7bfaf2f10e7c9aefe4b2353
Cesar Correchel <cesarcorrechel@gmail.com>
2019-03-03 23:09:46 -0600

    develop layout

--- src/components/Overview/Overview.js
+++ src/components/Overview/Overview.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import Online from './Widgets/Online/Online';
+import Popular from './Widgets/Popular/Popular';
+import Trends from './Widgets/Trends/Trends';
 
-const Overview = () => {
+const overview = props => {
+
   return(
-    
+    <div className = "overview">
+
+      <div className = "overview-item">
+
+        <Online />
+
+      </div>
+
+      <div className = "overview-item">
+
+        <Popular />
+
+      </div>
+
+      <div className = "overview-item">
+
+        <Trends />
+
+      </div>
+
+    </div>
   );
+
 }
+
+export default overview;
