6c2c73c731297df91cc52f1fe698d790db2a721b
Cesar Correchel <cesarcorrechel@gmail.com>
2019-03-10 23:53:06 -0600

    add svg icons to menu list - add cta component

--- src/components/Sidebar/Navigation/Navigation.js
+++ src/components/Sidebar/Navigation/Navigation.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import './Navigation.css';
+import CallToAction from '../../CallToAction/CallToAction';
+import { ReactComponent as DahsboardSVG } from '../../../assets/svgs/menu.svg';
+import { ReactComponent as StoriesSVG } from '../../../assets/svgs/story.svg';
+import { ReactComponent as AnalyticsSVG } from '../../../assets/svgs/chart.svg';
+import { ReactComponent as SettingsSVG } from '../../../assets/svgs/settings.svg';
 
 const Navigation = (props) => {
 
   return(
     <nav className = "nav-menu">
       <ul>
-        <li><a href="#">Dashboard</a></li>
-        <li><a href="#">Stories</a></li>
-        <li><a href="#">Analytics</a></li>
-        <li><a href="#">Settings</a></li>
+        <li><a href="#"><DahsboardSVG/> <span>Dashboard</span></a></li>
+        <li><a href="#"><StoriesSVG/> <span>Stories</span></a></li>
+        <li><a href="#"><AnalyticsSVG/> <span>Analytics</span></a></li>
+        <li><a href="#"><SettingsSVG/> <span>Settings</span></a></li>
       </ul>
+
+      <CallToAction />
     </nav>
   );
 
