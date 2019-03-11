f3c0c4664472dbd27cf86718b52fd4d278b17c66
Cesar Correchel <cesarcorrechel@gmail.com>
2019-03-10 23:51:54 -0600

    add call to action component

---
+++ src/components/CallToAction/CallToAction.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './CallToAction.css';
+
+const callToAction = props => {
+
+  return(
+    <div className = "premium-cta">
+      <h4>Premium</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras commodo.</p>
+      <a className = "btn" href="#">Upgrade</a>
+    </div>
+  );
+
+}
+
+export default callToAction;
