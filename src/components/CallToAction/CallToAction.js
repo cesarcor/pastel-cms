9d31f9bc7c0cbaa67d5a3f1a0ed9a2a797e690e9
Cesar Correchel <cesarcorrechel@gmail.com>
2019-03-11 23:46:00 -0600

    add basic styles for CTA

--- src/components/CallToAction/CallToAction.js
+++ src/components/CallToAction/CallToAction.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './CallToAction.css';
+import { ReactComponent as CrownSVG } from '../../assets/svgs/crown.svg';
 
 const callToAction = props => {
 
   return(
     <div className = "premium-cta">
+      <CrownSVG />
       <h4>Premium</h4>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras commodo.</p>
-      <a className = "btn" href="#">Upgrade</a>
+
+      <div>
+        <a className = "btn" href="#">Upgrade</a>
+      </div>
+
     </div>
   );
 
