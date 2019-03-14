b1835978d58bb18d86348bbb4070c43d25d8ae97
Cesar Correchel <cesarcorrechel@gmail.com>
2019-03-13 22:39:53 -0600

    change className for text

--- src/components/Overview/Widgets/Popular/Popular.js
+++ src/components/Overview/Widgets/Popular/Popular.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import popularIMG from '../../../../assets/images/img_1.jpg';
+import './Popular.css';
+import StoryItem from '../../../Stories/Stories/StoryItem';
 
 const popular = props => {
 
     return(
       <div className = "w-popular">
 
-        <h4 className = "secondary-heading">Popular Stories</h4>
-
+        <h4 className = "overview__text">Popular Stories</h4>
 
       </div>
     );
