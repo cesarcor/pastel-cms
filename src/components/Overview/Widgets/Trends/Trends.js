881028725461fbcf1134b09d63f66e7ea3c1030f
Cesar Correchel <cesarcorrechel@gmail.com>
2019-03-13 22:41:01 -0600

    add jsx/html structure for relevant indicators

--- src/components/Overview/Widgets/Trends/Trends.js
+++ src/components/Overview/Widgets/Trends/Trends.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import './Trends.css';
 
 const trends = props => {
 
     return(
       <div className = "w-trends">
 
-        <h4 className = "secondary-heading">Active Users Trending Over Time</h4>
-        
+        <h4 className = "overview__text">Active Users Trending Over Time</h4>
+
+        <div className = "trend-data__contain">
+
+          <div className = "trend-data-item">
+            <span>124</span>
+            <span>Stories</span>
+          </div>
+
+          <div className = "trend-data-item">
+            <span>14k</span>
+            <span>Views</span>
+          </div>
+
+          <div className = "trend-data-item">
+            <span>1m 21sec</span>
+            <span>Av. Session Duration</span>
+          </div>
+
+        </div>
+
 
       </div>
     );
