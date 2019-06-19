7d06076004fd53ee1fa8cbb0e7fa17ee5a8c9adc
Cesar Correchel <cesarcorrechel@gmail.com>
2019-06-18 23:32:46 -0600

    add notification component

---
+++ src/components/Notification/Notification.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Notification.css';
+
+const notification = props => {
+
+    return(<span className="notification">12</span>);
+    
+}
+
+export default notification;
