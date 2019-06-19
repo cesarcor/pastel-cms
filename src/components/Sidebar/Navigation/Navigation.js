bedc0cc44186990750e3a934c85fa726ca359347
Cesar Correchel <cesarcorrechel@gmail.com>
2019-06-18 23:32:22 -0600

    add comment menu item and add notification item

--- src/components/Sidebar/Navigation/Navigation.js
+++ src/components/Sidebar/Navigation/Navigation.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Navigation.css';
 import CallToAction from '../../CallToAction/CallToAction';
+import Notification from '../../Notification/Notification';
 import { ReactComponent as DahsboardSVG } from '../../../assets/svgs/menu.svg';
 import { ReactComponent as StoriesSVG } from '../../../assets/svgs/story.svg';
 import { ReactComponent as AnalyticsSVG } from '../../../assets/svgs/chart.svg';
 import { ReactComponent as SettingsSVG } from '../../../assets/svgs/settings.svg';
+import { ReactComponent as CommentSVG } from '../../../assets/svgs/comments.svg';
 
-const Navigation = (props) => {
+const Navigation = props => {
 
   return(
 
@@ -15,6 +17,7 @@ const Navigation = (props) => {
         <ul>
           <li><a href="#"><DahsboardSVG/> <span>Dashboard</span></a></li>
           <li><a href="#"><StoriesSVG/> <span>Stories</span></a></li>
+          <li><a href="#"><CommentSVG /> <span>Comments & Messages</span><Notification/></a></li>
           <li><a href="#"><AnalyticsSVG/> <span>Analytics</span></a></li>
           <li><a href="#"><SettingsSVG/> <span>Settings</span></a></li>
         </ul>
