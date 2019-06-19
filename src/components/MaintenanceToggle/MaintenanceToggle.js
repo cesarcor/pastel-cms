ce447fb209b13b42e58b498ff9df5891da756f34
Cesar Correchel <cesarcorrechel@gmail.com>
2019-06-18 23:31:40 -0600

    make maintenance toggledisplay inline

--- src/components/MaintenanceToggle/MaintenanceToggle.js
+++ src/components/MaintenanceToggle/MaintenanceToggle.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './MaintenanceToggle.css'
 
 const maintenanceToggle = props => {
 
 return(
-  <div>
+  <div className="maintenance-mode">
     <label>
     Maintenance Mode
     <input type="radio" />
