5349efa4795d0c36043c43e942144ca17133b46a
Cesar Correchel <cesarcorrechel@gmail.com>
2019-09-07 15:15:06 -0600

    change span attribute class to className

--- src/components/Overview/Widgets/Trends/Trends.js
+++ src/components/Overview/Widgets/Trends/Trends.js
@@ -12,17 +12,17 @@ const trends = props => {
 
           <div className = "trend-data-item">
             <span>124</span>
-            <span class="overview__text">Stories</span>
+            <span className="overview__text">Stories</span>
           </div>
 
           <div className = "trend-data-item">
             <span>14k</span>
-            <span class="overview__text">Views</span>
+            <span className="overview__text">Views</span>
           </div>
 
           <div className = "trend-data-item">
             <span>1m 21sec</span>
-            <span class="overview__text">Av. Session Duration</span>
+            <span className="overview__text">Av. Session Duration</span>
           </div>
 
         </div>
